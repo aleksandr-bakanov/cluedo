c158aa7cca5aee995bd8ffa2a8310b0e4abb4db4
Aleksandr Bakanov <bakanov.aleksandr@gmail.com>
2012-01-31 23:26:59 +0400

    Add three client scripts.

---
+++ client/python/cli3.py
@@ -0,0 +1,24 @@
+import socket
+
+HOST = 'localhost'  # The remote host
+PORT = 7003         # The same port as used by the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+
+data = s.recv(1024);
+print 'Received', repr(data)
+
+er = u'\x03\x00\x02\x00\x03'
+s.send(er)
+
+data = s.recv(1024);
+print 'Received', repr(data)
+
+er = u'\x03\x00\x06\x00\x03'
+s.send(er)
+
+while True:
+    data = s.recv(1024);
+    print 'Received', repr(data)
+
+s.close()
